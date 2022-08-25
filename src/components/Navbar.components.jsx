ea3a0ae1e9e5b2344f142dbb613846656b741797
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-08-24 22:01:04 -0400

    Changed Navigation Bar to Icons

--- src/components/Navbar.components.jsx
+++ src/components/Navbar.components.jsx
@@ -1,23 +1,25 @@
 import { Component } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import  { Github, Linkedin, FileEarmarkFill } from 'react-bootstrap-icons';
 import "bootstrap/dist/css/bootstrap.min.css";
 class Navigation extends Component {
   render() {
     return (
       <Navbar bg="transparent" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Eric Qiu</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home">About Me</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-            <Nav.Link href="#home">Resume</Nav.Link>
-            <Nav.Link href="#link">Contact Me</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Container>
+          <Navbar.Brand href="#home">Eric Qiu</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="https://github.com/ericqiu04" target="_blank">
+                <Github className = "icons"/>
+              </Nav.Link>
+              <Nav.Link href="#link"><Linkedin className = "icons"/></Nav.Link>
+              <Nav.Link href="#home"><FileEarmarkFill className = "icons"/></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
   }
 }
