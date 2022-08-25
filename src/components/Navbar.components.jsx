738c34e82688c81db73e620137411da147701142
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-08-24 22:05:58 -0400

    Added favicon to index and logo

--- src/components/Navbar.components.jsx
+++ src/components/Navbar.components.jsx
@@ -1,21 +1,34 @@
 import { Component } from "react";
+import Favicon from "./icons/favicon.png";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import  { Github, Linkedin, FileEarmarkFill } from 'react-bootstrap-icons';
+import { Github, Linkedin, FileEarmarkFill } from "react-bootstrap-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 class Navigation extends Component {
   render() {
     return (
       <Navbar bg="transparent" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Eric Qiu</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img
+              src={Favicon}
+              width="60"
+              height="60"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="https://github.com/ericqiu04" target="_blank">
-                <Github className = "icons"/>
+                <Github className="icons" />
               </Nav.Link>
-              <Nav.Link href="#link"><Linkedin className = "icons"/></Nav.Link>
-              <Nav.Link href="#home"><FileEarmarkFill className = "icons"/></Nav.Link>
+              <Nav.Link href="#link">
+                <Linkedin className="icons" />
+              </Nav.Link>
+              <Nav.Link href="#home">
+                <FileEarmarkFill className="icons" />
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
