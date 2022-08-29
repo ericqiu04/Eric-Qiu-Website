2a9a9931aee612ddeba99d117fbbd9f1ec1616b5
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-08-28 22:07:25 -0400

    added suit randomizer on card animation

--- src/components/Introduction.components.jsx
+++ src/components/Introduction.components.jsx
@@ -7,11 +7,17 @@ import "../CSS-Files/stackCard.css";
 class Introduction extends Component {
   render() {
     const suits = ["suithearts", "suitdiamonds", "suitspades", "suitclubs"];
-    const random = Math.floor(Math.random() * suits.length);
-    const rand1 = suits[random];
-    const rand2 = suits[random];
-    for (var i = 0; i < 9; i++) {}
-    console.log(rand1 + rand2);
+    const random = []
+    const cardSuit = [];
+
+    for(var i = 0; i <= 9; i++) {
+      random.push(Math.floor(Math.random() * suits.length))
+    }
+    for (var i = 0; i <= 9; i++) {
+      cardSuit.push(suits[random[i]]);
+    }
+    console.log(cardSuit);
+    
     return (
       <div className="intro" id="hero">
         <Row>
@@ -84,6 +90,48 @@ class Introduction extends Component {
           </Col>
           <Col className="animation" lg={3}>
             {/*Add card stacking animation*/}
+            <div className="loading">
+              <div style={{ "--i": "1" }} className={cardSuit[0]}>
+                <span>A</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "2" }} className={cardSuit[1]}>
+                <span>2</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "3" }} className={cardSuit[2]}>
+                <span>3</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "4" }} className={cardSuit[3]}>
+                <span>4</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "5" }} className={cardSuit[4]}>
+                <span>5</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "6" }} className={cardSuit[5]}>
+                <span>6</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "7" }} className={cardSuit[6]}>
+                <span>7</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "8" }} className={cardSuit[7]}>
+                <span>8</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "9" }} className={cardSuit[8]}>
+                <span>9</span>
+                <span></span>
+              </div>
+              <div style={{ "--i": "10" }} className={cardSuit[9]}>
+                <span>10</span>
+                <span></span>
+              </div>
+            </div>
           </Col>
         </Row>
       </div>
@@ -92,47 +140,3 @@ class Introduction extends Component {
 }
 
 export default Introduction;
-/*
-<div className="loading">
-              <div style={{ "--i": "1" }} className= {rand1}>
-                <span>A</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "2" }} className= {rand2}>
-                <span>2</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "3" }} className= {suits[random]}>
-                <span>3</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "4" }} className= {suits[random]}>
-                <span>4</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "5" }} className= {suits[random]}>
-                <span>5</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "6" }} className= {suits[random]}>
-                <span>6</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "7" }} className= {suits[random]}>
-                <span>7</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "8" }} className= {suits[random]}>
-                <span>8</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "9" }} className= {suits[random]}>
-                <span>9</span>
-                <span></span>
-              </div>
-              <div style={{ "--i": "10" }} className= {suits[random]}>
-                <span>10</span>
-                <span></span>
-              </div>
-            </div>
-            */
